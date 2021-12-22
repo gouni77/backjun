cbe70045a74480ee6b4e33ee28f5ff7300b7d9fe
goun <sgw7546@gmail.com>
2021-12-22 23:33:49 +0900

    add : 1001번 추가

---
+++ inoutput/6.js
@@ -0,0 +1,11 @@
+//1001번 A-B
+/* 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0<A,B<10)*/
+const fs = require("fs");
+const inputData = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split("")
+  .map((value) => +value);
+const [a, b] = inputData;
+console.log(a - b);
